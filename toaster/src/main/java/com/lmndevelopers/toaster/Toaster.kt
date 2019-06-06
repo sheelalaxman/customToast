975f4afaf715b17f585bff2952d73c658dbb3010
sheelalaxman <laxman.sheela251@gmail.com>
2019-06-06 11:49:46 +0530

    simple toast fixed

--- toaster/src/main/java/com/lmndevelopers/toaster/Toaster.kt
+++ toaster/src/main/java/com/lmndevelopers/toaster/Toaster.kt
@@ -3,10 +3,10 @@ package com.lmndevelopers.toaster
 import android.content.Context
 import android.widget.Toast
 
+class Toaster {
 
-class ToastMessage
-{
-    fun simple( c : Context, message: String) {
+    fun simple(c : Context,message : String){
         Toast.makeText(c,message,Toast.LENGTH_LONG).show()
     }
+    
 }
